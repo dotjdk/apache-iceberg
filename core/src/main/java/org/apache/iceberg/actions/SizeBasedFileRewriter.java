fbe5ae8e7fef048fcd6f89b409a233a0b067498e
Jan Justesen <janj@tlt.local>
2024-03-01 11:04:46 +0400

    Added support for
    * `include-files`: A comma separated list of files to include
    * `include-files-pattern`: A regex pattern that file paths must match

--- core/src/main/java/org/apache/iceberg/actions/SizeBasedFileRewriter.java
+++ core/src/main/java/org/apache/iceberg/actions/SizeBasedFileRewriter.java
@@ -19,9 +19,13 @@
 package org.apache.iceberg.actions;
 
 import java.math.RoundingMode;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import org.apache.iceberg.ContentFile;
 import org.apache.iceberg.ContentScanTask;
 import org.apache.iceberg.Table;
@@ -101,6 +105,10 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
 
   public static final long MAX_FILE_GROUP_SIZE_BYTES_DEFAULT = 100L * 1024 * 1024 * 1024; // 100 GB
 
+  public static final String INCLUDE_FILES = "include-files";
+
+  public static final String INCLUDE_FILES_PATTERN = "include-files-pattern";
+
   private static final long SPLIT_OVERHEAD = 5 * 1024;
 
   private final Table table;
@@ -110,6 +118,8 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
   private int minInputFiles;
   private boolean rewriteAll;
   private long maxGroupSize;
+  private List<String> includeFiles;
+  private Pattern includeFilesPattern;
 
   protected SizeBasedFileRewriter(Table table) {
     this.table = table;
@@ -133,7 +143,9 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
         MAX_FILE_SIZE_BYTES,
         MIN_INPUT_FILES,
         REWRITE_ALL,
-        MAX_FILE_GROUP_SIZE_BYTES);
+        MAX_FILE_GROUP_SIZE_BYTES,
+        INCLUDE_FILES,
+        INCLUDE_FILES_PATTERN);
   }
 
   @Override
@@ -147,6 +159,9 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
     this.rewriteAll = rewriteAll(options);
     this.maxGroupSize = maxGroupSize(options);
 
+    includeFiles = includeFiles(options);
+    includeFilesPattern = includeFilesPattern(options);
+
     if (rewriteAll) {
       LOG.info("Configured to rewrite all provided files in table {}", table.name());
     }
@@ -154,6 +169,11 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
 
   protected boolean wronglySized(T task) {
     return task.length() < minFileSize || task.length() > maxFileSize;
+  }
+
+  protected boolean includedFile(T task) {
+    return (includeFiles.isEmpty() || includeFiles.contains(task.file().path().toString()))
+            || Optional.ofNullable(includeFilesPattern).map(p -> p.matcher(task.file().path()).matches()).orElse(true);
   }
 
   @Override
@@ -317,5 +337,19 @@ public abstract class SizeBasedFileRewriter<T extends ContentScanTask<F>, F exte
 
   private boolean rewriteAll(Map<String, String> options) {
     return PropertyUtil.propertyAsBoolean(options, REWRITE_ALL, REWRITE_ALL_DEFAULT);
+  }
+
+  private static List<String> includeFiles(Map<String, String> options) {
+    return Arrays
+            .stream(PropertyUtil.propertyAsString(options, INCLUDE_FILES, "").split(","))
+            .map(String::trim)
+            .collect(Collectors.toList());
+  }
+
+  private static Pattern includeFilesPattern(Map<String, String> options) {
+    return Optional
+            .ofNullable(PropertyUtil.propertyAsString(options, INCLUDE_FILES_PATTERN, null))
+            .map(Pattern::compile)
+            .orElse(null);
   }
 }
